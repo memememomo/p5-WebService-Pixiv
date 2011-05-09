99460d0d8b0acf81fa453968eb12e3462658b921
memememomo <memememomo@gmail.com>
2011-05-09 21:12:03 +0900

    initial commit

---
+++ lib/WebService/Pixiv.pm
@@ -0,0 +1,32 @@
+package WebService::Pixiv;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+WebService::Pixiv -
+
+=head1 SYNOPSIS
+
+  use WebService::Pixiv;
+
+=head1 DESCRIPTION
+
+WebService::Pixiv is
+
+=head1 AUTHOR
+
+memememomo E<lt>memememmomo@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
